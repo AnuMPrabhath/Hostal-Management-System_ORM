b72efb4f2c2afd3d753bebe1a720363a6383e326
AnuMPrabhath <anushkaprabhath15@gmail.com>
2023-09-05 20:31:54 +0530

    create StartForm.fxml

---
+++ src/main/java/com/example/hostalmanagementsystem_orm/controller/StartFormController.java
@@ -0,0 +1,17 @@
+package com.example.hostalmanagementsystem_orm.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartFormController {
+
+    @FXML
+    private Button startBtn;
+
+    @FXML
+    void onAction(ActionEvent event) {
+
+    }
+
+}
