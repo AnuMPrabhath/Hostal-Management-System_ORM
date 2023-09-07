ce92493259e6152cfc354a0b1caeadd27c82addf
AnuMPrabhath <anushkaprabhath15@gmail.com>
2023-09-07 23:48:13 +0530

    create table module

---
+++ src/main/java/com/example/hostalmanagementsystem_orm/tm/UserTm.java
@@ -0,0 +1,17 @@
+package com.example.hostalmanagementsystem_orm.tm;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTm {
+    private String id;
+    private String password;
+    private String passwordHint;
+}
